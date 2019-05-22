9ca4bc73365b07ccbf393ec86e00b760864f0295
南风 <xueyj163@163.com>
2019-05-22 18:42:05 +0800

    update pom file

--- donkey-lite-generator/src/main/java/com/donkeycode/data/mapper/CourseMapper.java
+++
@@ -1,8 +0,0 @@
-package com.donkeycode.data.mapper;
-
-import com.donkeycode.boot.utils.BaseMapper;
-import com.donkeycode.data.domain.Course;
-import org.apache.ibatis.annotations.Select;
-
-public interface CourseMapper extends BaseMapper<Course> {
-}
