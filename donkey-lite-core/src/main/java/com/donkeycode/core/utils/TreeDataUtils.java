9c751459996933d488df4df43a10209ca6c4d243
Dean <xueyj163@163.com>
2019-06-28 17:13:18 +0800

    add Tree Utils

--- donkey-lite-core/src/main/java/com/donkeycode/core/utils/TreeDataUtils.java
+++ donkey-lite-core/src/main/java/com/donkeycode/core/utils/TreeDataUtils.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.donkeycode.core.entity.TreeNode;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
@@ -94,5 +95,66 @@ public class TreeDataUtils {
             });
         }
         return nodes;
+    }
+
+
+    /**
+     * 两层循环实现建树
+     *
+     * @param treeNodes 传入的树节点列表
+     * @return
+     */
+    public static <T extends TreeNode> List<T> bulid(List<T> treeNodes, Object root) {
+
+        List<T> trees = new ArrayList<T>();
+
+        for (T treeNode : treeNodes) {
+            if (root.equals(treeNode.getParentId())) {
+                trees.add(treeNode);
+            }
+            for (T it : treeNodes) {
+                if (it.getParentId() == treeNode.getId()) {
+                    if (treeNode.getChildren() == null) {
+                        treeNode.setChildren(new ArrayList<TreeNode>());
+                    }
+                    treeNode.add(it);
+                }
+            }
+        }
+        return trees;
+    }
+
+    /**
+     * 使用递归方法建树
+     *
+     * @param treeNodes
+     * @return
+     */
+    public static <T extends TreeNode> List<T> buildByRecursive(List<T> treeNodes, Object root) {
+        List<T> trees = new ArrayList<T>();
+        for (T treeNode : treeNodes) {
+            if (root.equals(treeNode.getParentId())) {
+                trees.add(findChildren(treeNode, treeNodes));
+            }
+        }
+        return trees;
+    }
+
+    /**
+     * 递归查找子节点
+     *
+     * @param treeNodes
+     * @return
+     */
+    public static <T extends TreeNode> T findChildren(T treeNode, List<T> treeNodes) {
+        for (T it : treeNodes) {
+            if (treeNode.getId() == it.getParentId()) {
+                if (treeNode.getChildren() == null) {
+                    treeNode.setChildren(new ArrayList<TreeNode>());
+                }
+                treeNode.add(findChildren(it, treeNodes));
+            }
+        }
+        return treeNode;
     }
 }
